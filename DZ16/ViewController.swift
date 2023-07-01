4b27f30d703cda23f8ee92f7e1fc5fdfb6c823bd
LexinIV <ileksin41@gmail.com>
2023-07-01 20:15:43 +0300

    Second commit

---
+++ DZ16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ16
+//
+//  Created by Yggdrasil on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
